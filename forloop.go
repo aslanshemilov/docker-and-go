7dc845727e2ee0988522914b6c1336006004c77a
S Kancharla <31664851+Shilpa-Kancharla@users.noreply.github.com>
2018-03-23 15:54:46 -0400

    Update forloop.go
    https://tour.golang.org/

--- forloop.go
+++ forloop.go
@@ -64,3 +64,51 @@ func sqrt(x float64) string {
 func main() {
 	fmt.Println(sqrt(2), sqrt(-4))
 }
+
+//If with a short statement
+//Like for, the if statement can start with a short statement to execute before the condition.
+//Variables declared by the statement are only in scope until the end of the if
+//Try using v in the last return statement.
+
+import (
+	"fmt"
+	"math"
+)
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	}
+	return lim
+}
+
+func main() {
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20),
+	)
+}
+
+//If and else statements
+
+import (
+	"fmt"
+	"math"
+)
+
+func pow(x, n, lim float64) float64 {
+	if v := math.Pow(x, n); v < lim {
+		return v
+	} else {
+		fmt.Printf("%g >= %g\n", v, lim)
+	}
+	// can't use v here, though
+	return lim
+}
+
+func main() {
+	fmt.Println(
+		pow(3, 2, 10),
+		pow(3, 3, 20), //trying changing around the last input on this one to see the effect of the else statement
+	)
+}
