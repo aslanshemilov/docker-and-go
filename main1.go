4675d21b13f7ad2617d4f39a264447d29110f5c6
S Kancharla <31664851+Shilpa-Kancharla@users.noreply.github.com>
2018-03-23 15:25:32 -0400

    Update main1.go

--- main1.go
+++ main1.go
@@ -63,3 +63,18 @@ func main() {
 	v := 42 // change me!
 	fmt.Printf("v is of type %T\n", v)
 }
+
+//Constants are declared like variables, but with the const keyword
+//Constants can be character, string, boolean, or numeric values
+//Constants cannot be declared using the := syntax
+
+const Pi = 3.14
+
+func main() {
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+}
