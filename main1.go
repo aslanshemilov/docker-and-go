1469a20bce784740820cd9e65d1a3c99cf8d434a
S Kancharla <31664851+Shilpa-Kancharla@users.noreply.github.com>
2018-03-23 15:14:59 -0400

    Update main1.go

--- main1.go
+++ main1.go
@@ -18,3 +18,17 @@ func main() {
 	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
 	fmt.Printf("Type: %T Value: %v\n", z, z)
 }
+
+//Variables declared without an explicit initial value are given their zero value. The zero value is:
+// `0` for numeric types
+// `false` for the boolean type, and
+// `""` (the empty string) for strings.
+
+func main() {
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
+
