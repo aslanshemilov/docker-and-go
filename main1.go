e4a9d3ece057da58025cceb8170cf8368b894a32
S Kancharla <31664851+Shilpa-Kancharla@users.noreply.github.com>
2018-03-23 15:23:14 -0400

    Update main1.go

--- main1.go
+++ main1.go
@@ -32,3 +32,29 @@ func main() {
 	fmt.Printf("%v %v %v %q\n", i, f, b, s)
 }
 
+//The expression T(v) converts the value v to the type T. Some numeric conversions:
+//var i int = 42
+//var f float64 = float64(i)
+//var u unit = uint(f)
+//Or, put more simply:
+//i := 42
+//f :=  float64(i)
+//u := uint(f)
+
+func main() {
+	var x, y int = 3, 4
+	var f float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f)
+	fmt.Println(x, y, z)
+}
+
+//When the RHS of the declaration is typed, the new variable is of that same type:
+//var i int
+//j := i //j is an int
+
+//However, when the RHS contains an untyped numeric constant, the new variable may be an int, float64, or complex128 depdending
+//on the precision of the constant:
+
+//i := 42 //int
+//f := 3.142 //float64
+//g := 0.867 + 0.5i //complex128
